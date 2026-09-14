717ab44511e501fbd23c8528e83fe012874ef888
tobbe <tobias.johansson.se@protonmail.com>
2026-09-14 20:13:04 +0200

    Bump version to 0.4.0 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "se.cloudsite.nextsign"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     signingConfigs {
